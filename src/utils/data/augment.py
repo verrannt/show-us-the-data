025583207db239d73087a0846209222201cc2c51
thomasroodnl <zakelijkthomasrood@hotmail.com>
2021-05-30 12:17:15 +0200

    Fix length_diff to be more intuitive, fix device assert bug

--- src/utils/data/augment.py
+++ src/utils/data/augment.py
@@ -38,74 +38,73 @@ class MentionSwapAugment(object):
                     if len(new_dataset) == len(ment_pos):
                         input_ids[ment_pos] = new_dataset
                     else:
-                        length_diff = len(ment_pos) - new_dataset_len
-                        #                         print(tokenizer.convert_ids_to_tokens(input_ids))
-                        #                         print(tags)
-                        #                         print(attention_mask)
-                        #                         print(ment_pos)
-
-                        if length_diff > 0:
-                            return self.augment_with_smaller_label(input_ids, tags, attention_mask, new_dataset,
-                                                                   ment_pos, length_diff, new_dataset_len)
+                        length_diff = new_dataset_len - len(ment_pos)
+#                         print(tokenizer.convert_ids_to_tokens(input_ids))
+#                         print(tags)
+#                         print(attention_mask)
+#                         print(ment_pos)
+                    
+                        if length_diff < 0:
+                            return self.augment_with_smaller_label(input_ids, tags, attention_mask, new_dataset, ment_pos, length_diff, new_dataset_len)
                         else:
-                            if torch.count_nonzero(input_ids) - length_diff <= len(input_ids):
-                                return self.augment_with_bigger_label(input_ids, tags, attention_mask, new_dataset,
-                                                                      ment_pos, length_diff, new_dataset_len)
+                            if ment_pos[0] + new_dataset_len <= len(input_ids):
+                                return self.augment_with_bigger_label(input_ids, tags, attention_mask, new_dataset, ment_pos, length_diff, new_dataset_len)
 
+                    
         return input_ids, tags, attention_mask
-
-    def augment_with_smaller_label(self, input_ids, tags, attention_mask, new_dataset, mention_positions, length_diff,
-                                   new_dataset_len):
+    
+    
+    def augment_with_smaller_label(self, input_ids, tags, attention_mask, new_dataset, mention_positions, length_diff, new_dataset_len):
         # Set new tokens
         new_token_positions = mention_positions[:new_dataset_len]
         input_ids[new_token_positions] = new_dataset
 
         # Roll back rest of sentence and tags
-        input_ids[new_token_positions[-1] + 1:-length_diff] = input_ids[
-                                                              new_token_positions[-1] + 1 + length_diff:].clone()
-        tags[new_token_positions[-1] + 1:-length_diff] = tags[new_token_positions[-1] + 1 + length_diff:].clone()
-        attention_mask[new_token_positions[-1] + 1:-length_diff] = attention_mask[
-                                                                   new_token_positions[-1] + 1 + length_diff:].clone()
+        input_ids[new_token_positions[-1]+1:length_diff] = input_ids[new_token_positions[-1] + 1 - length_diff:].clone()
+        tags[new_token_positions[-1]+1:length_diff] = tags[new_token_positions[-1] + 1 - length_diff:].clone()
+        attention_mask[new_token_positions[-1]+1:length_diff] = attention_mask[new_token_positions[-1] + 1 - length_diff:].clone()
 
         # Fill ends (padding and O token)
-        input_ids[-length_diff:] = 0
-        tags[-length_diff:] = self.o_id
-        attention_mask[-length_diff:] = 0
+        input_ids[length_diff:] = 0
+        tags[length_diff:] = self.o_id
+        attention_mask[length_diff:] = 0
 
-        #         print(tokenizer.convert_ids_to_tokens(input_ids))
-        #         print(tags)
-        #         print(attention_mask)
+#         print(tokenizer.convert_ids_to_tokens(input_ids))
+#         print(tags)
+#         print(attention_mask)
 
-        #         print("Smaller tag filled in")
-        #         raise Exception("FINISH")
-
+#         print("Smaller tag filled in")
+#         raise Exception("FINISH")
+        
         return input_ids, tags, attention_mask
-
-    def augment_with_bigger_label(self, input_ids, tags, attention_mask, new_dataset, mention_positions, length_diff,
-                                  new_dataset_len):
+    
+    
+    def augment_with_bigger_label(self, input_ids, tags, attention_mask, new_dataset, mention_positions, length_diff, new_dataset_len):
         # New token positions
-        new_token_positions = torch.arange(mention_positions[0], mention_positions[0] + new_dataset_len)
+        new_token_positions = torch.arange(mention_positions[0], mention_positions[0]+new_dataset_len)
+
 
         # Roll sentence away
-        input_ids[new_token_positions[-1] + 1:] = input_ids[mention_positions[-1] + 1:length_diff].clone()
-        tags[new_token_positions[-1] + 1:] = tags[mention_positions[-1] + 1:length_diff].clone()
-        attention_mask[new_token_positions[-1] + 1:] = attention_mask[mention_positions[-1] + 1:length_diff].clone()
+        input_ids[new_token_positions[-1]+1:] = input_ids[mention_positions[-1]+1:-length_diff].clone()
+        tags[new_token_positions[-1]+1:] = tags[mention_positions[-1]+1:-length_diff].clone()
+        attention_mask[new_token_positions[-1]+1:] = attention_mask[mention_positions[-1]+1:-length_diff].clone()
 
         # Set new tokens
         input_ids[new_token_positions] = new_dataset
-
+        
         # Continue with I token!
-        tags[mention_positions[-1] + 1:mention_positions[-1] + 1 - length_diff] = self.i_id
-        attention_mask[mention_positions[-1] + 1:mention_positions[-1] + 1 - length_diff] = 1
+        tags[mention_positions[-1]+1:mention_positions[-1]+1+length_diff] = self.i_id
+        attention_mask[mention_positions[-1]+1:mention_positions[-1]+1+length_diff] = 1
 
-        #         print(tokenizer.convert_ids_to_tokens(input_ids))
-        #         print(tags)
-        #         print(attention_mask)
+#         print(tokenizer.convert_ids_to_tokens(input_ids))
+#         print(tags)
+#         print(attention_mask)
 
-        #         print("Bigger tag filled in")
-        #         raise Exception("FINISH")
-
+#         print("Bigger tag filled in")
+#         raise Exception("FINISH")
+        
         return input_ids, tags, attention_mask
+        
 
 
 """
